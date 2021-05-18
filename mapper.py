20cf0c50d48aed2b90dfccbeecfdade3a01279b7
heterogenousok <82374551+heterogenousok@users.noreply.github.com>
2021-05-18 15:53:46 +0800

    Add files via upload

---
+++ mapper.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+#Author:luke
+#email:daxuelu@foxmail.com
+
+import sys
+
+#输入为标准输入stdin
+for line in sys.stdin:
+    #删除开头和结尾的空行
+    line = line.strip()
+    #以默认空格分隔单词到words列表
+    words = line.split()
+    for word in words:
+        #输出所有单词，格式为“单词 1”以便作为Reduce的输入
+        print("%s %s"%(word,1))
